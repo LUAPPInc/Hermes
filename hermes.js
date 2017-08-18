bdef566a59864e43b62b68739dbf9d52e611c284
Lucas Santos <lhs.santoss@gmail.com>
2017-08-18 18:39:52 -0300

    :truck: Renamed main file

---
+++ hermes.js
@@ -0,0 +1,16 @@
+const cli = require('caporal')
+const meta = require('./package.json')
+
+cli
+  .version(meta.version)
+  .description(meta.description)
+
+cli
+  .command('viewport', 'Starts a Hermes viewport test')
+  .argument('[links]', 'List of links to be tested (comma separated)', cli.LIST)
+  .option('-l, --links-file <path-to-json>', 'Path to a json containing the links to be tested', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt }, './screenshots')
+  .option('-o, --output <path>', 'Output path for the screenshots', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt }, './screenshots')
+  .option('-v, --viewports-file <path-to-json>', 'Path to a viewport configuration file', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt })
+  .action(require('./src/commands/test-viewport.command'))
+
+cli.parse(process.argv)
