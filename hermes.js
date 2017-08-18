3dbb6b4b161c5372edb4a7b05732a284b3dbc50f
Lucas Santos <lhs.santoss@gmail.com>
2017-08-18 19:34:46 -0300

    :bug: Fixed option name with verbose name

--- hermes.js
+++ hermes.js
@@ -11,7 +11,7 @@ cli
   .argument('[links]', 'List of links to be tested (comma separated)', cli.LIST)
   .option('-l, --links-file <path-to-json>', 'Path to a json containing the links to be tested', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt })
   .option('-o, --output <path>', 'Output path for the screenshots', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt }, './screenshots')
-  .option('-v, --viewports-file <path-to-json>', 'Path to a viewport configuration file', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt })
+  .option('-vp, --viewports-file <path-to-json>', 'Path to a viewport configuration file', (opt) => { if (typeof opt !== 'string') { throw new Error('Path must be a string') } return opt })
   .action(require('./src/commands/test-viewport.command'))
 
 cli.parse(process.argv)
